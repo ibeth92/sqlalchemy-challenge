00e161a12d5e8e2bf8c793fa1536b5b1b33d59b0
Ibet Hernandez <ibethernandez92@gmail.com>
2020-10-05 14:01:50 -0500

    fixes

--- analysis/Climate App.py
+++ analysis/Climate App.py
@@ -33,7 +33,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return(
-        f"Welcome to the Hawaii Climate API!<br/>"
+        f"Hawaii Climate API<br/>"
         f"Avaialble Routes:<br>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
@@ -43,25 +43,42 @@ def welcome():
 # Set up Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+# Create our session (link) from Python to the DB
+    session = Session(engine)
 
 # Query Measurement date and Precipitation
-    precip_data = session.query(func.strftime("%Y-%m-%d, Measurement.date), Measurement.prcp).filter(func.strftime("%Y-%m-%d, Measurement.date) >= dt.date(2016-8-23)).all()
-    precip_data
-    precipitation = {date:prcp for date, prcp in precip_data}
-    return jsonify(precipitation)
+    results =   session.query(Measurement.date, Measurement.prcp).\
+                order_by(Measurement.date).all()
+
+# Convert to list of dictionaries to jsonify
+    precip_date_list = []
+
+    for date, prcp in results:
+        new_dict = {}
+        new_dict[date] = precip
+        precip_date_list.append(new_dict)
+
+    session.close()
+
+    return jsonify(precip_date_list)
 
 # Set up Stations
 @app.route("/api/v1.0/stations")
 def stations():
 
 # Create our session (link) from Python to the DB
-        session = Session(engine)
+    session = Session(engine)
 
-# Query Stations
-    stations_all = session.query(Station.station).all()
+    stations = {}
+
+# Query all stations
+    results = session.query(Station.station, Station.name).all()
+    for sta, name in results:
+        stations[sta] = name
+
     session.close()
-    stations_list = list(np.ravel(stations_all))
-    return jsonify(stations_list)
+ 
+    return jsonify(stations)
 
 # Set up Tobs
 @app.route("/api/v1.0/tobs")
@@ -73,7 +90,7 @@ def tobs():
 # Query all Temperatures for the most active station ID
     temp_active = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date).all()
     session.close()
-    temp_list = list(np.ravel(temp_active)
+    temp_list = list(np.ravel(temp_active))
     return jsonify(temp_list)
 
 # Set up start and end
@@ -88,15 +105,15 @@ def tempdata(start, end):
 
     if type(endate) is not datetime.date:
         endate = "2020-08-31"
-        sel = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
-        stats = session.query(*sel).fliter(Measurement.date >=startdate).all()
+        select = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
+        stats = session.query(*select).fliter(Measurement.date >=startdate).all()
         results = list(np.ravel(stats))
         return jsonify(results)
 
     if type(startdate) is not datetime.date:
         startdate = "1992-08-31"
-        sel = [func.min(Measurement.tobs), func.max(Measurement.tobs),func.avg(Measurement.tobs)]
-        stats = session.query(*sel).filter(Measurement.date >=startdate).filter(Measurement.date >=endate).all()
+        select = [func.min(Measurement.tobs), func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+        stats = session.query(*select).filter(Measurement.date >=startdate).filter(Measurement.date >=endate).all()
         results = list(np.ravel(stats))
         session.close()
         return jsonify(results)
